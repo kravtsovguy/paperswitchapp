765e9e72816d9eaf56bfd5e17b3dea04b6d3683a
Матвей Кравцов <matvey.kravtsov.e@gmail.com>
2016-04-04 15:17:28 +0300

    Initial Commit

---
+++ PaperSwitchApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PaperSwitchApp
+//
+//  Created by Матвей Кравцов on 04.04.16.
+//  Copyright © 2016 Matvey Kravtsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
