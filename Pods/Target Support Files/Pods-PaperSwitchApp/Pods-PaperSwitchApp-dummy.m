6299ec74a41ebb2ddfd08f219f00e64cbe73a32b
Матвей Кравцов <matvey.kravtsov.e@gmail.com>
2016-04-04 15:55:51 +0300

    v1

---
+++ Pods/Target Support Files/Pods-PaperSwitchApp/Pods-PaperSwitchApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PaperSwitchApp : NSObject
+@end
+@implementation PodsDummy_Pods_PaperSwitchApp
+@end
---
+++ Pods/Target Support Files/Pods-PaperSwitchApp/Pods-PaperSwitchApp-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_PaperSwitchAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PaperSwitchAppVersionString[];
+
