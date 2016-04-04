685a5ec1e0002a7a0dd844262f93268a99b889f8
Матвей Кравцов <matvey.kravtsov.e@gmail.com>
2016-04-04 15:52:34 +0300

    v1

--- PaperSwitchApp/ViewController.swift
+++ PaperSwitchApp/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import RAMPaperSwitch
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mSwitch: RAMPaperSwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
